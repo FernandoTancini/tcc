41cf00265c162b496eba0781fe41d600c4fd9939
Fernando Tancini <fernandotancini@MBP-de-Fernando.box>
2021-09-23 18:18:49 -0300

    .

--- sample_app/base/management/commands/create_data.py
+++ sample_app/base/management/commands/create_data.py
@@ -1,13 +1,45 @@
-from base.models import
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+from base.models import Disease, Dog, Vaccine
 
 class Command(BaseCommand):
-    help = 'Create default admin user'
+    help = 'Create db initial data'
 
     def handle(self, *args, **options):
-        print('Creating superuser...')
-        if User.objects.filter(username='admin').exists():
-            print('Already created!')
-            return
+        admin_user = User.objects.filter(username='admin').first()
+        if not admin_user:
+            admin_user = User.objects.create_superuser('admin', 'admin@example.com', 'adm')
 
-        User.objects.create_superuser('admin', 'admin@example.com', 'adm')
-        print('Successfully created!')
+        dogs = []
+        for i in range(10):
+            dog = Dog.objects.create(
+                name = f'Dog {i}',
+                age = i,
+                owner = admin_user)
+            dogs.append(dog)
+
+        vaccines = []
+        for i in range(10):
+            vaccine = Vaccine.objects.create(
+                name = f'Vaccine {i}')
+            vaccines.append(vaccine)
+
+        diseases = []
+        for i in range(10):
+            disease = Disease.objects.create(
+                name = f'Disease {i}')
+            diseases.append(disease)
+
+        # m2m fields
+
+        for dog in dogs:
+            other_dogs = [d for d in dogs if d != dog]
+            dog.dog_friends.add(*other_dogs)
+
+        for dog in dogs:
+            dog.vaccines.add(*vaccines)
+
+        for vaccine in vaccines:
+            vaccine.covered_diseases.add(*diseases)
+
--- sample_app/sample_app/schema.py
+++ sample_app/sample_app/schema.py
@@ -1,17 +1,14 @@
-from graphene_django.schema import DjangoSchema
-from django.db.models.fields import BooleanField
 import graphene
 from base.models import Disease, Dog, Vaccine
 from django.contrib.auth.models import User
 from django.db.models import Value
+from django.db.models.fields import BooleanField
+# from graphene.relay import Node
 from graphene_django.debug import DjangoDebug
+# from graphene_django.fields import DjangoConnectionField
+# from graphene_django.filter.fields import DjangoFilterConnectionField
+from graphene_django.schema import DjangoSchema
 from graphene_django.types import DjangoObjectType
-
-
-class UserType(DjangoObjectType):
-    class Meta:
-        model = User
-        fields = ['owned_dogs']
     
 
 class DogType(DjangoObjectType):
@@ -19,13 +16,11 @@ class DogType(DjangoObjectType):
         model = Dog
         fields = ['name', 'age', 'dog_friends', 'vaccines']
 
-    is_pretty = graphene.Boolean()
-    def annotate_is_pretty(qs, _info):
-        return qs.annotate(is_pretty=Value(True, output_field=BooleanField()))
 
-    is_prettyy = graphene.NonNull(graphene.List(graphene.NonNull(graphene.Boolean)))
-    def resolve_is_prettyy(self, _info):
-        return [True, False]
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ['owned_dogs']
 
 
 class VaccineType(DjangoObjectType):
@@ -42,10 +37,10 @@ class DiseaseType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name="_debug")
-    profile = graphene.Field(UserType)
+    user = graphene.Field(UserType)
 
-    def resolve_profile(root, info):
+    def resolve_user(root, info):
         return info.context.user
 
 
-schema = DjangoSchema(query=Query)
+schema = DjangoSchema(query=Query, automatic_preparation=True)
